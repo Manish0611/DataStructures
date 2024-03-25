76795711acdd4132fea5ded1a714705fe9fd651a
Dikshithula Sai Manish <144203644+Manish0611@users.noreply.github.com>
2024-03-25 15:39:08 +0530

    Update common prefix.java

--- common prefix.java
+++ common prefix.java
@@ -1,39 +1,14 @@
+
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-       if (strs == null || strs.length == 0) return "";
-
-        List<char[]> charArrays = new ArrayList<>();
-        for (String str : strs) {
-            charArrays.add(str.toCharArray());
-        }
-
-        StringBuilder commonPrefix = new StringBuilder();
-        char[] firstWordChars = charArrays.get(0);
-
-        for (int i = 0; i < firstWordChars.length; i++) {
-            char ch = firstWordChars[i];
-            boolean match = true;
-
-            for (int j = 1; j < charArrays.size(); j++) {
-                char[] chars = charArrays.get(j);
-
-                if (i >= chars.length || chars[i] != ch) {
-                    match = false;
-                    break;
-                }
-            }
-
-            if (match) {
-                commonPrefix.append(ch);
-                
-            }
-            else{
-                break;
-            }
-
-            
-        }
-
-        return commonPrefix.toString();
-    }
-    }
+      Arrays.sort(strs);
+      StringBuilder result=new StringBuilder();
+      char[] first=strs[0].toCharArray();
+      char[] last=strs[strs.length-1].toCharArray();
+      for(int i=0;i<first.length;i++){
+        if(first[i]!=last[i]){
+            break;}
+            result.append(first[i]);
+        
+      }
+      return result.toString();   }}
